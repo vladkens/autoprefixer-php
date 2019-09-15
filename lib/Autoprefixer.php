583ee99a6714a5a72df0c4ce4fbc18297e35a984
Alex <ulkoalex@gmail.com>
2019-09-15 21:34:50 +0300

    check written and read data

--- lib/Autoprefixer.php
+++ lib/Autoprefixer.php
@@ -7,127 +7,141 @@
 
 class Autoprefixer
 {
-    /**
-     * @var array
-     */
-    private $browsers = array();
-    
-    /**
-     * @param   mixed   $browsers
-     */
-    public function __construct($browsers = null)
-    {
-        if (!is_null($browsers)) {
-            $this->setBrowsers($browsers);
-        }
-    }
-    
-    /**
-     * Set browsers info.
-     * @param   mixed   $browsers   String if one argument, array if many.
-     * @return  void
-     */
-    public function setBrowsers($browsers)
-    {
-        if (!is_array($browsers)) {
-            $browsers = array($browsers);
-        }
-        $this->browsers = $browsers;
-    }
+	/**
+	 * @var array
+	 */
+	private $browsers = array();
 
-    /**
-     * @param   mixed   $css
-     * @param   mixed   $browsers
-     * @throws  RuntimeException        If node runtime unavailable
-     * @throws  AutoprefixerException
-     * @return  array
-     */
-    public function compile($css, $browsers = null)
-    {
+	/**
+	 * @param   mixed   $browsers
+	 */
+	public function __construct($browsers = null)
+	{
+		if (!is_null($browsers)) {
+			$this->setBrowsers($browsers);
+		}
+	}
+
+	/**
+	 * Set browsers info.
+	 * @param   mixed   $browsers   String if one argument, array if many.
+	 * @return  void
+	 */
+	public function setBrowsers($browsers)
+	{
+		if (!is_array($browsers)) {
+			$browsers = array($browsers);
+		}
+		$this->browsers = $browsers;
+	}
+
+	/**
+	 * @param   mixed   $css
+	 * @param   mixed   $browsers
+	 * @throws  RuntimeException        If node runtime unavailable
+	 * @throws  AutoprefixerException
+	 * @return  array
+	 */
+	public function compile($css, $browsers = null)
+	{
         if ($return_string = !is_array($css)) {
-            $css = array($css);
-        }
-        
-        $data_string = json_encode(array(
-            'css' => $css,
+			$css = array($css);
+		}
+
+		$data_string = json_encode(array(
+			'css'      => $css,
             'browsers' => !is_null($browsers) ? $browsers : $this->browsers)
         );
 
-        if($data_string === false || $data_string === null) {
-            $error_message = 'Failed to json_encode: ';
-            if (function_exists('json_last_error_msg')) {
-                $error_message .= json_last_error_msg();
-            } else {
-                $error_message .= json_last_error();
-            }
-            throw new AutoprefixerException($error_message);
-        }
+		if ($data_string === false || $data_string === null) {
+			$error_message = 'Failed to json_encode: ';
+			if (function_exists('json_last_error_msg')) {
+				$error_message .= json_last_error_msg();
+			} else {
+				$error_message .= json_last_error();
+			}
+			throw new AutoprefixerException($error_message);
+		}
 
-        $nodejs = proc_open('node ' . __DIR__ . '/vendor/wrap.js',
+		$nodejs = proc_open('node ' . __DIR__ . '/vendor/wrap.js',
 			array(array('pipe', 'r'), array('pipe', 'w'), array('file', '/tmp/autoprefixer.error.log', 'a')),
-            $pipes
-        );
-        if ($nodejs === false) {
-            throw new RuntimeException('Could not reach node runtime');
-        }
+			$pipes
+		);
+		if ($nodejs === false) {
+			throw new RuntimeException('Could not reach node runtime');
+		}
 
-        $this->fwrite_stream($pipes[0], $data_string);
-        fclose($pipes[0]);
+		$written = $this->fwrite_stream($pipes[0], $data_string);
+		fclose($pipes[0]);
 
-        $output = stream_get_contents($pipes[1]);
-        $output = json_decode($output, true);
-        fclose($pipes[1]);
-        
-        proc_close($nodejs);
-        
-        $error_messages = '';
-        foreach ($output as $key => &$value) {
-            if (preg_match('/^Error:\s*/i', $value)) {
-                $value = preg_replace('/^Error:\s*/i', '', $value);
-                $error_messages .= "In css[$key]: $value \n";
-            }
-        }
-        
-        if (strlen($error_messages) > 0) {
-            throw new AutoprefixerException($error_messages);
-        }
-        
-        return $return_string ? $output[0] : $output;
-    }
-    
-    /**
-     * Download autoprefixer updates.
-     * @return  bool    True if updated.
-     */
-    public function update()
-    {
-        $update_url = 'https://raw.github.com/ai/autoprefixer-rails/master/vendor/autoprefixer.js';
-        $local_path = __DIR__ . '/vendor/autoprefixer.js';
-        $new = file_get_contents($update_url);
-        $old = file_get_contents($local_path);
-        
-        if (md5($new) == md5($old)) return false;
-        
-        file_put_contents($local_path, $new);
-        return true;
-    }
-    
-    /**
-     * @param   object  $fp         php://stdin
-     * @param   string  $string
-     * @param   int     $buflen
-     * @return  string
-     */
-    private function fwrite_stream($fp, $string, $buflen = 4096)
-    {
-        for ($written = 0, $len = strlen($string); $written < $len; $written += $fwrite) {
-            $fwrite = fwrite($fp, substr($string, $written, $buflen));
-            if ($fwrite === false) {
-                return $written;
-            }
-        }
-        
-        return $written;
-    }
-    
+		if ($written !== strlen($data_string)) {
+			proc_close($nodejs);
+			throw new AutoprefixerException(sprintf('Could not write data: %d/%d', $written, strlen($data_string)));
+		}
+
+		$output = stream_get_contents($pipes[1]);
+		proc_close($nodejs);
+
+		if (!$output) {
+			// $stat = proc_get_status($nodejs);
+			// ini_set( 'xdebug.overload_var_dump', 'off' );
+			// var_dump($output, $stat);exit;
+			throw new AutoprefixerException('Could not read output');
+		}
+		$output = json_decode($output, true);
+
+		if (!is_array($output)) {
+			throw new AutoprefixerException('Broken output');
+		}
+
+		$error_messages = '';
+		foreach ($output as $key => &$value) {
+			if (preg_match('/^Error:\s*/i', $value)) {
+				$value = preg_replace('/^Error:\s*/i', '', $value);
+				$error_messages .= "In css[$key]: $value \n";
+			}
+		}
+
+		if (strlen($error_messages) > 0) {
+			throw new AutoprefixerException($error_messages);
+		}
+
+		return $return_string ? $output[0] : $output;
+	}
+
+	/**
+	 * Download autoprefixer updates.
+	 * @return  bool    True if updated.
+	 */
+	public function update()
+	{
+		$update_url = 'https://raw.github.com/ai/autoprefixer-rails/master/vendor/autoprefixer.js';
+		$local_path = __DIR__ . '/vendor/autoprefixer.js';
+		$new = file_get_contents($update_url);
+		$old = file_get_contents($local_path);
+
+		if (md5($new) == md5($old)) return false;
+
+		file_put_contents($local_path, $new);
+		return true;
+	}
+
+	/**
+	 * @param   object  $fp         php://stdin
+	 * @param   string  $string
+	 * @param   int     $buflen
+	 * @return  string
+	 */
+	private function fwrite_stream($fp, $string, $buflen = 4096)
+	{
+		for ($written = 0, $len = strlen($string); $written < $len; $written += $fwrite) {
+			$fwrite = fwrite($fp, substr($string, $written, $buflen));
+			if ($fwrite === false) {
+				return $written;
+			}
+		}
+
+		return $written;
+	}
+
 };
